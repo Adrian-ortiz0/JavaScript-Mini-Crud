0d7d86c0fcc541daf566575428d5595e8fcdb493
Adrian-ortiz0 <dxniel7328@gmail.com>
2024-07-30 10:56:36 -0500

    feat: :sparkles: Se agrega la funcion de menu

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 let usuarios = []
 let userId = 1
+
+function menu(){
+    window.alert("Bienvenido a campusApp")
+    while (true) {
+        opt = window.prompt("Que deseas hacer? \n 1. Registrar Usuarios \n 2. Listar Usuarios \n 3. Salir")
+        
+        if (opt === "1") {
+            window.alert("Registro")
+        } else if (opt === "2"){
+            window.alert("Listar")
+        } else if (opt === "3"){
+            window.alert("Saliendo...")
+            break
+        }
+    }
+
+}
+
+menu()
